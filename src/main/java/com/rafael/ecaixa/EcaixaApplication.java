8cbd2dd5fc886b68b86497c51f7f4eecd22ff220
RaLiotti <rafa.liotti@gmail.com>
2021-08-16 22:45:47 -0300

    iniciando o projeto

---
+++ src/main/java/com/rafael/ecaixa/EcaixaApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.ecaixa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcaixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcaixaApplication.class, args);
+	}
+
+}
